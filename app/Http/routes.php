493401aa272d5991b16a259d99d500adbb3e1e6d
Eray AYDIN <eray@labkod.com>
2016-02-12 22:40:50 +0200

    Added sticker index page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,9 +2,11 @@
 
 Route::group(['middleware' => ['web']], function () {
 
-    Route::group(['namespace' => 'Frontend'], function(){
+    Route::group(['namespace' => 'Frontend', 'as' => 'frontend.'], function(){
 
         Route::get("/", ["as" => "home.index", "uses" => "HomeController@index"]);
+
+        Route::get("sticker", ["as" => "sticker.index", "uses" => "StickerController@index"]);
 
     });
 
---
+++ resources/views/sticker/index.blade.php
@@ -0,0 +1,229 @@
+@extends("layout.master")
+
+@section("body")
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 sidebar">
+                    <h3><a href="#">Yazılım</a></h3>
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">AngularJS</a>
+                            <ul class="nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">ng-conf</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">ng-europe</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Git</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">JavaScript</a>
+                            <ul class="nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                    <h3><a href="#">Yazılım</a></h3>
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">AngularJS</a>
+                            <ul class="nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">ng-conf</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">ng-europe</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Git</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">JavaScript</a>
+                            <ul class="nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Backbone.js</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-9 main">
+                    <form class="search">
+                        <div class="input-group input-group-lg">
+                            <input type="text" class="form-control" placeholder="Sticker Arayın...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-secondary text-primary" type="button"><i class="fa fa-search"></i></button>
+                            </span>
+                        </div>
+                    </form>
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/php.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">PHP</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/chrome.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">Google Chrome</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/linux.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">Linux Tux</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/ruby.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">Ruby</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/php.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">PHP</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/chrome.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">Google Chrome</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/linux.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">Linux Tux</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/ruby.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">Ruby</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/php.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">PHP</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/chrome.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">Google Chrome</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/linux.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">Linux Tux</h4>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="card card-inverse sticker">
+                                <img class="card-img-top img-fluid" src="http://devstickers.com/assets/img/cat/ruby.png" alt="Card image cap">
+                                <div class="card-img-overlay">
+                                    <h4 class="card-title text-center">Ruby</h4>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <nav class="text-center">
+                                <ul class="pagination">
+                                    <li class="page-item disabled">
+                                        <a class="page-link" href="#" aria-label="Previous">
+                                            <span aria-hidden="true">&laquo;</span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                    </li>
+                                    <li class="page-item active">
+                                        <a class="page-link" href="#">1 <span class="sr-only">(current)</span></a>
+                                    </li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">4</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">5</a></li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#" aria-label="Next">
+                                            <span aria-hidden="true">&raquo;</span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@stop
