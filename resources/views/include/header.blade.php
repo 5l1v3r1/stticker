ab1e8aadfb5f7b39d88c35cf9e0ebfc81c7a83ab
Eray AYDIN <eray@labkod.com>
2016-02-13 04:09:13 +0200

    Added user links to header, added icons to home and sticker links

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -11,16 +11,19 @@
             <div class="col-md-8">
                 <nav class="nav nav-inline nav-primary text-md-right text-sm-center text-xs-center">
                     <div class="nav-item">
-                        <a class="nav-link" href="#">Anasayfa</a>
+                        <a class="nav-link" href="#"><i class="fa fa-home"></i> Anasayfa</a>
                     </div>
                     <div class="nav-item">
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Sticker</a>
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-cubes"></i> Sticker</a>
                         <div class="dropdown-menu">
                             <a class="dropdown-item" href="#"><i class="fa fa-code"></i> Yazılım</a>
                             <a class="dropdown-item" href="#"><i class="fa fa-film"></i> Film</a>
                             <a class="dropdown-item" href="#"><i class="fa fa-video-camera"></i> Dizi</a>
                             <a class="dropdown-item" href="#"><i class="fa fa-gamepad"></i> Oyun</a>
                         </div>
+                    </div>
+                    <div class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-user"></i> Eray Aydın</a>
                     </div>
                     <div class="nav-item">
                         <a class="nav-link" href="#"><i class="fa fa-sign-in"></i> Giriş Yap</a>
@@ -30,6 +33,9 @@
                     </div>
                     <div class="nav-item">
                         <a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i> Sepetim (0)</a>
+                    </div>
+                    <div class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-sign-out"></i> Çıkış Yap</a>
                     </div>
                 </nav>
             </div>
