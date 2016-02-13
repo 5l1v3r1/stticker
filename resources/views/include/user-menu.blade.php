d53132cbc173bfe570c9c15d1b1fbb07570990f4
Eray AYDIN <eray@labkod.com>
2016-02-13 16:32:16 +0200

    Added routes to user menu

--- resources/views/include/user-menu.blade.php
+++ resources/views/include/user-menu.blade.php
@@ -1,8 +1,8 @@
 <div class="sidebar">
-    <h3><a href="#">Eray Aydın</a></h3>
+    <h3><a href="{{ route("frontend.user.settings") }}">{{ auth()->user()->fullname }}</a></h3>
     <ul class="nav">
         <li class="nav-item">
-            <a class="nav-link active" href="#">Hesap Ayarları</a>
+            <a class="nav-link @if(\Request::is("user/settings")) active @endif" href="{{ route("frontend.user.settings") }}">Hesap Ayarları</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">Adres Bilgilerim</a>
@@ -11,7 +11,7 @@
             <a class="nav-link" href="#">Siparişlerim</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Çıkış Yap</a>
+            <a class="nav-link" href="{{ route("frontend.user.logout") }}">Çıkış Yap</a>
         </li>
     </ul>
 </div>
