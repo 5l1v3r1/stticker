cb6702dc6e5aaacabd6dfecce27ae2267eca9ba1
Eray AYDIN <eray@labkod.com>
2016-02-13 05:39:40 +0200

    Added extra user fields to users table

---
+++ database/migrations/2016_02_13_033121_add_github_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGithubToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string("github")->nullable()->after("twitter");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn("github");
+        });
+    }
+}
