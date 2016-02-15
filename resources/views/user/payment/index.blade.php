a93707a1a8cdf3811b384f4df7f55f15d187d291
Eray AYDIN <eray@labkod.com>
2016-02-15 02:12:32 +0200

    Added status fields to user payment index and show

--- resources/views/user/payment/index.blade.php
+++ resources/views/user/payment/index.blade.php
@@ -25,7 +25,19 @@
                             <td>{{ $payment->created_at->format("d.m.Y - H:i") }}</td>
                             <td class="text-xs-center"><i class="fa fa-try"></i> {{ number_format($payment->total+$payment->cargo, 2) }}</td>
                             <td class="text-xs-center">
-                                <span class="label label-info">Kargoda</span>
+                                @if($payment->status == 0)
+                                    <span class="label label-danger">Ödeme Bekliyor</span>
+                                @elseif($payment->status == 1)
+                                    <span class="label label-info">Sipariş Alındı</span>
+                                @elseif($payment->status == 2)
+                                    <span class="label label-primary">Kargoda</span>
+                                @elseif($payment->status == 3)
+                                    <span class="label label-success">Teslim Edildi</span>
+                                @elseif($payment->status == 4)
+                                    <span class="label label-danger">İptal Edildi</span>
+                                @elseif($payment->status == 5)
+                                    <span class="label label-warning">İade</span>
+                                @endif
                             </td>
                             <td class="text-xs-right">
                                 <a href="{{ route("frontend.user.payment.show", $payment->id) }}" class="btn btn-sm btn-primary-outline"><i class="fa fa-eye"></i> Sipariş Detayı</a>
--- resources/views/user/payment/show.blade.php
+++ resources/views/user/payment/show.blade.php
@@ -26,7 +26,19 @@
                             <td>{{ $payment->created_at->format("d.m.Y - H:i") }}</td>
                             <td class="text-xs-center"><i class="fa fa-try"></i> {{ number_format($payment->total + $payment->cargo, 2) }}</td>
                             <td class="text-xs-center">
-                                <span class="label label-info">Kargoda</span>
+                                @if($payment->status == 0)
+                                    <span class="label label-danger">Ödeme Bekliyor</span>
+                                @elseif($payment->status == 1)
+                                    <span class="label label-info">Sipariş Alındı</span>
+                                @elseif($payment->status == 2)
+                                    <span class="label label-primary">Kargoda</span>
+                                @elseif($payment->status == 3)
+                                    <span class="label label-success">Teslim Edildi</span>
+                                @elseif($payment->status == 4)
+                                    <span class="label label-danger">İptal Edildi</span>
+                                @elseif($payment->status == 5)
+                                    <span class="label label-warning">İade</span>
+                                @endif
                             </td>
                         </tr>
                         </tbody>
